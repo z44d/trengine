b6cb302135fdbfbc7388b1d711a24749b45e7c03
Zaid <zaid@192.168.1.6>
2024-05-18 20:16:09 +0300

    Support OCR from bytes

--- trengine/ocr.py
+++ trengine/ocr.py
@@ -3,81 +3,87 @@ import aiohttp
 import json
 import os
 
-from aiofiles import open as AsyncOpen
 
 from .exceptions import ApiException
+from typing import Union
 
 
 class OCR:
     def __init__(self) -> None:
         pass
 
-    def from_image(self, path: str, language: str = "eng") -> str:
-        with open(path, "rb") as f:
-            response = requests.post(
-                "https://api8.ocr.space/parse/image",
-                data={
-                    "language": language,
-                    "isOverlayRequired": True,
-                    "OCREngine": 1,
-                    "detectCheckbox": False,
-                    "IsCreateSearchablePDF": False,
-                    "isSearchablePdfHideTextLayer": True,
-                    "FileType": ".AUTO",
-                },
-                headers={
-                    "Apikey": "donotstealthiskey_ip1",
-                },
-                files={"file": f},
-            )
-            try:
-                result = response.json()
-            except Exception as e:
-                raise BaseException(str(e))
+    def from_image(self, path: Union[str, bytes], language: str = "eng") -> str:
+        if isinstance(path, bytes):
+            b = path
+        else:
+            with open(path, "rb") as f:
+                b = f.read()
+        response = requests.post(
+            "https://api8.ocr.space/parse/image",
+            data={
+                "language": language,
+                "isOverlayRequired": True,
+                "OCREngine": 1,
+                "detectCheckbox": False,
+                "IsCreateSearchablePDF": False,
+                "isSearchablePdfHideTextLayer": True,
+                "FileType": ".AUTO",
+            },
+            headers={
+                "Apikey": "donotstealthiskey_ip1",
+            },
+            files={"file": b},
+        )
+        try:
+            result = response.json()
+        except Exception as e:
+            raise BaseException(str(e))
 
-            if isinstance(result, str):
-                raise ApiException(result)
-            if not result.get("ParsedResults"):
-                raise ApiException(json.dumps(result, indent=4, ensure_ascii=False))
+        if isinstance(result, str):
+            raise ApiException(result)
+        if not result.get("ParsedResults"):
+            raise ApiException(json.dumps(result, indent=4, ensure_ascii=False))
 
-            return result["ParsedResults"][0]["ParsedText"]
+        return result["ParsedResults"][0]["ParsedText"]
 
 
 class AsyncOCR:
     def __init__(self) -> None:
         pass
 
-    async def from_image(self, path: str, language: str = "eng") -> str:
-        async with AsyncOpen(path, "rb") as f:
-            async with aiohttp.ClientSession(
-                headers={"Apikey": "donotstealthiskey_ip1"}
-            ) as session:
-                payload = {
-                    "language": language,
-                    "isOverlayRequired": True,
-                    "OCREngine": 1,
-                    "detectCheckbox": False,
-                    "IsCreateSearchablePDF": False,
-                    "isSearchablePdfHideTextLayer": True,
-                    "FileType": ".AUTO",
-                }
-                data = aiohttp.formdata.FormData(quote_fields=False)
-                for k, v in payload.items():
-                    data.add_field(k, str(v))
-                data.add_field("file", await f.read(), filename=os.path.basename(path))
-                async with session.post(
-                    "https://api8.ocr.space/parse/image", data=data
-                ) as response:
-                    try:
-                        result = await response.json()
-                    except Exception as e:
-                        raise BaseException(str(e))
+    async def from_image(self, path: Union[str, bytes], language: str = "eng") -> str:
+        if isinstance(path, bytes):
+            b = path
+        else:
+            with open(path, "rb") as f:
+                b = f.read()
+        async with aiohttp.ClientSession(
+            headers={"Apikey": "donotstealthiskey_ip1"}
+        ) as session:
+            payload = {
+                "language": language,
+                "isOverlayRequired": True,
+                "OCREngine": 1,
+                "detectCheckbox": False,
+                "IsCreateSearchablePDF": False,
+                "isSearchablePdfHideTextLayer": True,
+                "FileType": ".AUTO",
+            }
+            data = aiohttp.formdata.FormData(quote_fields=False)
+            for k, v in payload.items():
+                data.add_field(k, str(v))
+            data.add_field("file", b, filename=os.path.basename(path))
+            async with session.post(
+                "https://api8.ocr.space/parse/image", data=data
+            ) as response:
+                try:
+                    result = await response.json()
+                except Exception as e:
+                    raise BaseException(str(e))
 
-                    if isinstance(result, str):
-                        raise ApiException(result)
-                    if not result.get("ParsedResults"):
-                        raise ApiException(
-                            json.dumps(result, indent=4, ensure_ascii=False)
-                        )
+                if isinstance(result, str):
+                    raise ApiException(result)
+                if not result.get("ParsedResults"):
+                    raise ApiException(json.dumps(result, indent=4, ensure_ascii=False))
 
-                    return result["ParsedResults"][0]["ParsedText"]
+                return result["ParsedResults"][0]["ParsedText"]
